7e6090412dadc6f1eca8992eb6521bff20ca71cb
dominicpam89 <dominicpam89@gmail.com>
2024-04-05 17:45:07 +0700

    standardize pubchem-response.interceptor.ts

--- src/pubchem/interceptors/pubchem-response.interceptor.ts
+++ src/pubchem/interceptors/pubchem-response.interceptor.ts
@@ -1,25 +1,21 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FullRecordsData } from './../types/full-record.type';
-import { PropertyData, TProperties } from './../types/property.type';
-import { SynonymData } from './../types/synonym.type';
-import { FullRecordsDtoMake } from '../dtos/full-record.dto';
+import { TFullRecordsData } from './../types/full-record.type';
+import { TPropertyData } from './../types/property.type';
+import { TSynonymData } from './../types/synonym.type';
+import { findValueByKey } from '../dtos/full-record.dto';
 
-type TypeData = FullRecordsData | PropertyData | SynonymData;
-
-// Don't change it, it's coming from PubChem
+type TypeData = TFullRecordsData | TPropertyData | TSynonymData;
+/** 
+  the enum value, got from first key in each every TypeData
+  the purpose is to get what kind of data coming from original response:
+  can be FullRecordsData, TPropertyData, or TSynonymData
+*/
 const EnumData = {
   fullRecords: 'PC_Compounds',
   property: 'PropertyTable',
-  synonym: 'Information',
-};
-const getDataType = (data: TypeData): string => {
-  if (Object.keys(data).includes(EnumData.fullRecords))
-    return EnumData.fullRecords;
-  if (Object.keys(data).includes(EnumData.property)) return EnumData.property;
-  if (Object.keys(data).includes(EnumData.synonym)) return EnumData.synonym;
-  else return String('Interceptor error, data is unknown!');
+  synonym: 'InformationList',
 };
 
 export class PubchemResponseInterceptor implements NestInterceptor {
@@ -31,18 +27,16 @@ export class PubchemResponseInterceptor implements NestInterceptor {
     return next.handle().pipe(
       map((data: TypeData) => {
         let transformedData: any;
-        switch (getDataType(data)) {
+        switch (this.getDataType(data)) {
           // incase data type is full records
           case EnumData.fullRecords:
-            const compounds = data as FullRecordsData;
-            transformedData = FullRecordsDtoMake(compounds);
+            transformedData = this.interceptFullRecords(
+              data as TFullRecordsData,
+            );
             break;
           // incase data type is based on propertiesTable operation
           case EnumData.property:
-            const {
-              PropertyTable: { Properties },
-            } = data as PropertyData;
-            transformedData = this.interceptProperties(Properties);
+            transformedData = this.interceptProperties(data as TPropertyData);
           // incase data type is based on synonyms operation
           case EnumData.synonym:
             // temporary, need to update
@@ -53,24 +47,38 @@ export class PubchemResponseInterceptor implements NestInterceptor {
       }),
     );
   }
-  interceptProperties(properties: TProperties[]) {
+  interceptFullRecords(data: TFullRecordsData) {
+    const compoundData = data.PC_Compounds[0];
+    const pk: number = findValueByKey(data, 'cid');
+    const indexProp = compoundData.props.findIndex(
+      (prop) => prop.urn.label === 'SMILES',
+    );
+    const smiles: string = compoundData.props[indexProp]?.value?.sval;
+    return {
+      pk,
+      smiles,
+    };
+  }
+  interceptProperties(TPropertyData: TPropertyData) {
+    const {
+      PropertyTable: { Properties },
+    } = TPropertyData;
     // temporary
-    return properties;
+    return Properties;
     /** 
-      function to make clear output type and data based on
-      one cid request, and one property request
+      DTO to make clear output type and data based on
+      one property request
      */
     /** 
-      function to make clear output type and data based on
-      one cid request, and multiple properties request
+      DTO to make clear output type and data based on
+      multiple properties request
      */
-    /** 
-      function to make clear output type and data based on
-      multiple cid request, and one property request
-     */
-    /** 
-      function to make clear output type and data based on
-      multiple cid request, and multiple properties request
-     */
+  }
+  getDataType(data: TypeData): string {
+    if (Object.keys(data).includes(EnumData.fullRecords))
+      return EnumData.fullRecords;
+    if (Object.keys(data).includes(EnumData.property)) return EnumData.property;
+    if (Object.keys(data).includes(EnumData.synonym)) return EnumData.synonym;
+    else return String('Interceptor error, data is unknown!');
   }
 }
